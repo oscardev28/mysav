7b56212c5e890bbae3218a98bc1fb354413f1b59
oscardev28 <oscardevelop28@gmail.com>
2025-03-09 12:29:57 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,6 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  console.log("App Angular cargando en iPhone...");
+
